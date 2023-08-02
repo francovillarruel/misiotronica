b81237750b697a5bb3c4c9c40907391e8e401c90
Franco Villarruel <89492469+francovillarruel@users.noreply.github.com>
2023-08-01 23:21:38 -0300

    Update index.js

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,48 +1,55 @@
-import LOGO from "../../imagenes/LOGO.png"
-import React from 'react'
-import {Link} from 'react-router-dom'
-import { DataContext } from "../../Context/Dataprovider"
-import { useContext } from "react"
+ 
+import React, { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { DataContext } from '../../Context/Dataprovider';
+import LOGO from '../../imagenes/LOGO.png';
  
 export const Header = () => {
-    const value= useContext(DataContext);
-    const [menu,setMenu] = value.menu;
-    const [carrito]= value.carrito;
- 
-    const toogleMenu = () =>{
-        setMenu(!menu)
-    }
- 
+  const value = useContext(DataContext);
+  const [menu, setMenu] = value.menu;
+  const [carrito] = value.carrito;
 
-    return (
-   <header>
-    <Link to='/'>
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <header className={`sidebar ${showMenu ? 'active' : ''}`}>
+      {/* Aquí está el botón del menú hamburguesa */}
+      <div className='hamburger-menu' onClick={toggleMenu}>
+        <box-icon name={showMenu ? 'x' : 'menu'}></box-icon>
+      </div>
+
+      <Link to="/">
         <div className='logo'>
-            <img src={LOGO} alt="" height="120" />
+          <img src={LOGO} alt='' height='120' />
         </div>
-    </Link>
-    <ul>
-        <li>
+      </Link>
+
+      {/* El menú lateral */}
+      <div className='links'>
+        <ul>
+          <li>
             <Link to="/">INICIO</Link>
-        </li>
-        <li>
-            <Link to="productos">PRODUCTOS</Link>
-        </li>
-        <li>
+          </li>
+          <li>
+            <Link to="productos">VENTA</Link>
+          </li>
+          <li>
             <Link to="sobrenosotros">SOBRE NOSOTROS</Link>
-        </li>
-        <li>
+          </li>
+          <li>
             <Link to="contacto">CONTACTO</Link>
-        </li>
-        
-    </ul>
-    <div className="cart" onClick={toogleMenu}>
-        <box-icon name="cart"></box-icon>
-        <span className="item__total">{carrito.length}</span>
-    </div>
-   </header>
-  )
-}
+          </li>
+        </ul>
+      </div>
 
-
- 
+      <div className='cart' onClick={() => setMenu(!menu)}>
+        <box-icon name='cart'></box-icon>
+        <span className='item__total'>{carrito.length}</span>
+      </div>
+    </header>
+  );
+};
