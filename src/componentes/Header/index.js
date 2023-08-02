4ae1d2e9cac4c0853e4eefd03c4efd3947519180
Franco Villarruel <89492469+francovillarruel@users.noreply.github.com>
2023-08-02 15:52:47 -0300

    Update index.js

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,9 +1,8 @@
- 
 import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { DataContext } from '../../Context/Dataprovider';
 import LOGO from '../../imagenes/LOGO.png';
- 
+
 export const Header = () => {
   const value = useContext(DataContext);
   const [menu, setMenu] = value.menu;
@@ -19,7 +18,9 @@ export const Header = () => {
     <header className={`sidebar ${showMenu ? 'active' : ''}`}>
       {/* Aquí está el botón del menú hamburguesa */}
       <div className='hamburger-menu' onClick={toggleMenu}>
-        <box-icon name={showMenu ? 'x' : 'menu'}></box-icon>
+        <div className='bar'></div>
+        <div className='bar'></div>
+        <div className='bar'></div>
       </div>
 
       <Link to="/">
@@ -29,21 +30,23 @@ export const Header = () => {
       </Link>
 
       {/* El menú lateral */}
-      <div className='links'>
-        <ul>
-          <li>
-            <Link to="/">INICIO</Link>
-          </li>
-          <li>
-            <Link to="productos">VENTA</Link>
-          </li>
-          <li>
-            <Link to="sobrenosotros">SOBRE NOSOTROS</Link>
-          </li>
-          <li>
-            <Link to="contacto">CONTACTO</Link>
-          </li>
-        </ul>
+      <div className={`hambuger-menu-links ${showMenu ? 'showMenu' : ''}`}>
+        <div className='links'>
+          <ul>
+            <li>
+              <Link to="/">INICIO</Link>
+            </li>
+            <li>
+              <Link to="productos">VENTA</Link>
+            </li>
+            <li>
+              <Link to="sobrenosotros">SOBRE NOSOTROS</Link>
+            </li>
+            <li>
+              <Link to="contacto">CONTACTO</Link>
+            </li>
+          </ul>
+        </div>
       </div>
 
       <div className='cart' onClick={() => setMenu(!menu)}>
